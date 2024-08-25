1e49e7a68139669f1e9b1b89bbb3dcdb21e58b95
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2024-08-25 12:44:20 -0400

    v0.0.4 create and read 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 //import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import prisma from "@/lib/prisma"
+//import prisma from "@/lib/prisma"
 //import Image from "next/image";
 
 export default async function Home() {
 
-  const ListaProductos = await prisma.product.findMany()
+  //const ListaProductos = await prisma.product.findMany()
   
   return (
     <>
+      home
       {/* <div>
         {ListaProductos.map(ListaProductos => (
           <Card key={ListaProductos.id}>
